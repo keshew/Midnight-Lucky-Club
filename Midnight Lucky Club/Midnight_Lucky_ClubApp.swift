fc73c6a9eee9f2e2a9ec8a98104704e22eb0543c
keshew <artyom.korotckov2017@yandex.ru>
2026-05-13 14:20:57 +0500

    Initial Commit

---
+++ Midnight Lucky Club/Midnight_Lucky_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midnight_Lucky_ClubApp.swift
+//  Midnight Lucky Club
+//
+//  Created by Артём Коротков on 13.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Midnight_Lucky_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
